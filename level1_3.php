7c2f3ae7f83e1bc1cd78d4a2ec528d371ab826db
annacherneckaja <annacherneckaja@gmail.com>
2020-10-11 20:16:15 +0300

    level1

--- level1_3.php
+++ level1_3.php
@@ -1,9 +1,8 @@
 <?php
-// не обращайте на эту функцию внимания
-// она нужна для того чтобы правильно считать входные данные
+
 function readHttpLikeInput()
 {
-    $f = fopen('stdin2', 'r');
+    $f = fopen('stdin3', 'r');
     $store = "";
     $toread = 0;
     while ($line = fgets($f)) {
@@ -68,3 +67,59 @@ function getHeaders($array)
 $http = parseTcpStringAsHttpRequest($contents);
 
 echo(json_encode($http, JSON_PRETTY_PRINT));
+
+function processHttpRequest($method, $uri, $headers, $body)
+{
+    if ($method == "GET") {
+        if (preg_match("~^/sum~", $uri)) {
+            if (preg_match_all("~^/sum\?nums=(\d+(,(\d)+)*)$~",
+                $uri, $out, PREG_PATTERN_ORDER)) {
+                $status_code = 200;
+                $body_response =array_sum(explode(",", $out[1][0]));
+            } else {
+                $status_code = 400;
+            }
+        } else $status_code = 404;
+    } else $status_code = 400;
+    $status_message = getStatusMessage($status_code);
+    $headers_response = getHeadersResponse($body_response);
+
+    outputHttpResponse($status_code, $status_message, $headers_response, $body_response);
+}
+
+function getHeadersResponse($body_response)
+{
+    var_dump($body_response);
+    return array(
+        "Server" => "Apache / 2.2.14 (Win32)",
+        "Content - Length" => $body_response != NULL? strlen(json_encode($body_response)): 0,
+        "Connection" => "Closed",
+        "Content - Type" => "text / html;charset = utf - 8",
+    );
+}
+
+function getStatusMessage($status_code)
+{
+    switch ($status_code) {
+        case 400:
+            return "Bad Request";
+        case 404:
+            return "Not found";
+        case 200:
+            return "Ok";
+        default:
+            return "";
+    }
+}
+
+function outputHttpResponse($status_code, $status_message, $headers, $body)
+{
+    echo "<br> HTTP/1.1 " . "$status_code" . " " . "$status_message<br>";
+    echo date('l jS \of F Y h:i:s A') . "<br>";
+    foreach ($headers as $k => $v) {
+        echo "$k" . ": " . "$v<br>";
+    }
+    echo $body != null ? "<br> $body" : "";
+}
+
+processHttpRequest($http["method"], $http["uri"], $http["headers"], $http["body"]);
---
+++ level1_4.php
@@ -0,0 +1,137 @@
+<?php
+
+function readHttpLikeInput()
+{
+    $f = fopen('stdin4', 'r');
+    $store = "";
+    $toread = 0;
+    while ($line = fgets($f)) {
+        $store .= preg_replace("/\r/", "", $line);
+        if (preg_match('/Content-Length: (\d+)/', $line, $m))
+            $toread = $m[1] * 1;
+        if ($line == "\r\n")
+            break;
+    }
+    if ($toread > 0)
+        $store .= fread($f, $toread);
+    return $store;
+}
+
+$contents = readHttpLikeInput();
+
+function parseTcpStringAsHttpRequest($string)
+{
+    $arr = explode("\n", $string);
+    return array(
+        "method" => getMethod($arr[0]),
+        "uri" => getURI($arr[0]),
+        "headers" => getHeaders($arr),
+        "body" => ltrim($arr[array_search("", $arr) + 1]),
+    );
+}
+
+/**
+ * Searches subject for a match to the regular expression given in pattern.
+ * @param $string - the input string;
+ * @return int
+ */
+function getMethod($string)
+{
+    $requestMethods = "/GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH/";
+    preg_match($requestMethods, $string, $match);
+    return $match[0];
+}
+
+function getURI($string)
+{
+// https://docs.microsoft.com/en-us/previous-versions/msp-n-p/ff650303(v=pandp.10)?redirectedfrom=MSDN
+    $URI_Syntax = '~\/[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/)*)~'; //https://mathiasbynens.be/demo/url-regex
+    preg_match($URI_Syntax, $string, $match);
+    return $match[0];
+}
+
+function getHeaders($array)
+{
+    $headers = array();
+    for ($i = 1; $i < count($array); $i++) {
+        if ($array[$i] == "\r") break;
+        if (preg_match("/:/", $array[$i])) {
+            $header_name = explode(":", $array[$i])[0];
+            $header_value = explode(":", $array[$i])[1];
+            $headers[$header_name] = trim($header_value);
+        }
+    }
+    return $headers;
+}
+
+$http = parseTcpStringAsHttpRequest($contents);
+//$http = parseTcpStringAsHttpRequest($contents);
+
+echo(json_encode($http, JSON_PRETTY_PRINT));
+
+function processHttpRequest($method, $uri, $headers, $body)
+{
+    if ($method == "POST") {
+        if (preg_match("~^/api/checkLoginAndPassword$~", $uri) &&
+            $headers["Content-Type"] == "application/x-www-form-urlencoded") {
+            if (checkLoginAndPassword($body)) {
+                $status_code = 200;
+                $body_response = "FOUND";
+            } else {
+                $status_code = 403;
+                $body_response = "incorrect login or password";
+            }
+        } else $status_code = 404;
+    } else $status_code = 400;
+    $status_message = getStatusMessage($status_code);
+    $headers_response = getHeadersResponse($body_response);
+
+    outputHttpResponse($status_code, $status_message, $headers_response, $body_response);
+}
+
+function checkLoginAndPassword($body)
+{
+    $parsed_body = preg_split("~[&=]~", $body);
+    if (count($parsed_body) != 4) return false;
+    $login_and_password = "~" . $parsed_body[1] . ":" . $parsed_body[3] . "~";
+    return preg_match($login_and_password, file_get_contents('passwords.txt'));
+}
+
+function getHeadersResponse($body_response)
+{
+    return array(
+        "Server" => "Apache / 2.2.14 (Win32)",
+        "Content - Length" => strlen(json_encode($body_response)),
+        "Connection" => "Closed",
+        "Content - Type" => "text / html;charset = utf - 8",
+    );
+}
+
+
+function getStatusMessage($statuscode)
+{
+    switch ($statuscode) {
+        case 400:
+            return "Bad Request";
+        case 403:
+            return "Forbidden";
+        case 404:
+            return "Not found";
+        case 200:
+            return "Ok";
+        default:
+            return "";
+    }
+}
+
+function outputHttpResponse($status_code, $status_message, $headers, $body)
+{
+    echo "<br> HTTP/1.1 " . "$status_code" . " " . "$status_message<br>";
+    echo date('l jS \of F Y h:i:s A') . "<br>";
+    foreach ($headers as $k => $v) {
+        echo "$k" . ": " . "$v<br>";
+    }
+    echo $body != null ? "<br> $body" : "";
+}
+
+processHttpRequest($http["method"], $http["uri"], $http["headers"], $http["body"]);
