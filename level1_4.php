459921d53f846933d612b97a75ba9a364efba1db
annacherneckaja <annacherneckaja@gmail.com>
2020-10-12 09:57:48 +0300

    level1

--- level1_4.php
+++ level1_4.php
@@ -30,11 +30,6 @@ function parseTcpStringAsHttpRequest($string)
     );
 }
 
-/**
- * Searches subject for a match to the regular expression given in pattern.
- * @param $string - the input string;
- * @return int
- */
 function getMethod($string)
 {
     $requestMethods = "/GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH/";
@@ -71,10 +66,13 @@ echo(json_encode($http, JSON_PRETTY_PRINT));
 
 function processHttpRequest($method, $uri, $headers, $body)
 {
+    $body_response = "";
     if ($method == "POST") {
         if (preg_match("~^/api/checkLoginAndPassword$~", $uri) &&
             $headers["Content-Type"] == "application/x-www-form-urlencoded") {
-            if (checkLoginAndPassword($body)) {
+            if (!include 'passwords.txt') {
+                $status_code = 500;
+            } elseif (checkLoginAndPassword($body)) {
                 $status_code = 200;
                 $body_response = "FOUND";
             } else {
@@ -83,10 +81,10 @@ function processHttpRequest($method, $uri, $headers, $body)
             }
         } else $status_code = 404;
     } else $status_code = 400;
-    $status_message = getStatusMessage($status_code);
+    include 'statusMessage';
     $headers_response = getHeadersResponse($body_response);
 
-    outputHttpResponse($status_code, $status_message, $headers_response, $body_response);
+    outputHttpResponse($status_code,$status_massage_array[$status_code], $headers_response, $body_response);
 }
 
 function checkLoginAndPassword($body)
@@ -107,23 +105,6 @@ function getHeadersResponse($body_response)
     );
 }
 
-
-function getStatusMessage($statuscode)
-{
-    switch ($statuscode) {
-        case 400:
-            return "Bad Request";
-        case 403:
-            return "Forbidden";
-        case 404:
-            return "Not found";
-        case 200:
-            return "Ok";
-        default:
-            return "";
-    }
-}
-
 function outputHttpResponse($status_code, $status_message, $headers, $body)
 {
     echo "<br> HTTP/1.1 " . "$status_code" . " " . "$status_message<br>";
@@ -131,7 +112,7 @@ function outputHttpResponse($status_code, $status_message, $headers, $body)
     foreach ($headers as $k => $v) {
         echo "$k" . ": " . "$v<br>";
     }
-    echo $body != null ? "<br> $body" : "";
+    echo $body;
 }
 
 processHttpRequest($http["method"], $http["uri"], $http["headers"], $http["body"]);
